55238ffe1543bdb4ac5fc9238ad9203af2ae91d9
Piotr Rusiecki <me@peterrusiecki.eu>
2022-07-17 22:49:22 +0200

    Multiconnection

--- new_client/client.py
+++ new_client/client.py
@@ -1,11 +1,72 @@
+import sys
 import socket
+import selectors
+from tarfile import _Fileobj
+import types
+
+from hexapod_code_v2.new_server.server import service_connection
 
 ip = "192.168.178.55"
 port = 8050
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((ip, port))
-    s.sendall(b"Hello!")
-    data = s.recv(1024)
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
 
-print(f"Received: {data!r}")
+def start_connections(ip, port, num_conns):
+    server_addr = (ip, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid = connid,
+            msg_total = sum(len(m) for m in messages),
+            recv_total = 0,
+            messages = messages.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+def service_connection(key, mask):
+    sock = key._Fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg.total:
+            print(f"Closing connection {data.connid}")
+            sel.unregister(sock)
+            sock.close()
+        if mask & selectors.EVENT_WRITE:
+            if not data.outb and data.messages:
+                data.outb = data.messages.pop(0)
+            if data.outb:
+                print(f"Sending {data.outb!r} to connection {data.connid}")
+                sent = sock.send(data.outb)
+                data.outb = data.outb[sent:]
+
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+    sys.exit(1)
+
+num_conns = 2
+start_connections(host, int(port), int(num_conns))
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
--- new_server/server.py
+++ new_server/server.py
@@ -1,5 +1,7 @@
-import socket
 
+import socket
+import selectors
+import types
 #ip = socket.gethostbyname(socket.gethostname())
 ip = "0.0.0.0"
 port = 8050
@@ -7,14 +9,52 @@ port = 8050
 print(f"IP Address: {ip}")
 print(f"Port: {port}")
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(((ip, port)))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connecte3d by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+sel= selectors.DefaultSelector()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((ip,port))
+lsock.listen()
+
+print(f"Listening on {(ip, port)}")
+
+lsock.setblocking(False)
+
+sel.register(lsock, selectors.EVEN_READ, data=None)
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+            
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
