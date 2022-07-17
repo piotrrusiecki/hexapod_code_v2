bb783327e7be3e3e5ab9a13e32775268c81a63fc
Piotr Rusiecki <me@peterrusiecki.eu>
2022-07-17 19:51:55 +0200

    Basic client / server

---
+++ new_client/client.py
@@ -0,0 +1,11 @@
+import socket
+
+ip = "192.168.1.0"
+port = 8050
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connecT((ip, port))
+    s.sendall(b"Hello!")
+    data = s.recv(1024)
+
+print(f"Received: {data!r}")
---
+++ new_server/server.py
@@ -0,0 +1,19 @@
+import socket
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 8050
+
+print(f"IP Address: {ip}")
+print(f"Port: {port}")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(((ip, port)))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connecte3d by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
